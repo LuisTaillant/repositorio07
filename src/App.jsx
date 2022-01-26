7c4d3da5dcce79c3d33f4f80b5d5c49812f86dfa
Luis Taillant <luis.marcelo.taillant@gmail.com>
2022-01-26 19:05:53 -0300

    Desafio-DetalleProducto

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <ItemListContainer greetings="soy una props" />
+      {/*<ItemListContainer greetings="soy una props" />*/}
+      <ItemDetailContainer />
     </div>
   );
 }
---
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ItemDetail = (props) => {
+  const { name, category, stock, price, imag } = props.product;
+  return (
+    <>
+      <h1>{name}</h1>
+      <h4>Category: {category}</h4>
+      <h4>Stock: {stock}</h4>
+      <h4>Price: ${price}</h4>
+      <img src={imag} alt={name} />
+    </>
+  );
+};
+
+export default ItemDetail;
